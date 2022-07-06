3c74c95f022f2d946be47c5586ac33fd031fa0f0
frankbaileyjr <frankbaileyjr@gmail.com>
2022-07-05 21:29:13 -0400

    Completed Speed Variable

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // public or private reference?
+    // data type (int, float, bool, string)
+    // every variable has a name
+    // optional value assigned
+    [SerializeField]
+    private float _speed = 3.5f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // new Vector3(-1,0,0)
+        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+    }
+}
