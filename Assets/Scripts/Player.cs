aa339deb30b574b7687df691e20e4ca020dd7f47
frankbaileyjr <frankbaileyjr@gmail.com>
2022-07-06 18:05:06 -0400

    Finished Create the Player module

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
     // every variable has a name
     // optional value assigned
     [SerializeField]
-    private float _speed = 3.5f; 
+    private float _speed = 3.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // new Vector3(-1,0,0)
-        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        calculateMovement();
+    }
+
+    void calculateMovement()
+    {
+        // get movement
+        float horiztonalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horiztonalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0));
+
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
     }
 }
