ac42847ccf3807068848b03245e334fbe72b7d20
frankbaileyjr <frankbaileyjr@gmail.com>
2022-11-29 18:43:07 -0500

    working on boss script

--- Assets/Scripts/Enemies/Boss1.cs
+++ Assets/Scripts/Enemies/Boss1.cs
@@ -28,9 +28,47 @@ public class Boss1 : MonoBehaviour
     // health bar = 50?
 
     // Start is called before the first frame update
+
+    [SerializeField]
+    private bool _isPhaseOne = true;
+
+    [SerializeField]
+    GameObject _EBPrefab;
+
+    [SerializeField]
+    GameObject _bossBoltPrefab;
+
+    [SerializeField]
+    GameObject _megaLaserChargePrefab;
+
+    [SerializeField]
+    GameObject _texturedMegaLaserPrefab;
+
     void Start()
     {
-        
+
+        if (_EBPrefab == null)
+        {
+            Debug.Log("_EB on Boss1 is null!");
+        }
+
+        if (_bossBoltPrefab == null)
+        {
+            Debug.Log("_bossBoltPrefab on Boss1 is null!");
+        }
+
+        if (_megaLaserChargePrefab == null)
+        {
+            Debug.Log("_megaLaserChargePrefab on Boss1 is null!");
+        }
+
+        if (_texturedMegaLaserPrefab == null)
+        {
+            Debug.Log("_texturedMegaLaserPrefab on Boss1 is null!");
+        }
+
+
+
     }
 
     // Update is called once per frame
@@ -38,4 +76,17 @@ public class Boss1 : MonoBehaviour
     {
         
     }
+
+    IEnumerator _phaseOne ()
+    {
+        yield return new WaitForSeconds(0.50f);
+    }
+
+    IEnumerator _phaseTwo()
+    {
+        yield return new WaitForSeconds(0.50f);
+    }
+
+
+
 }
