95e29aad9450b6075d7403f0e1b0398dc09d003a
frankbaileyjr <frankbaileyjr@gmail.com>
2022-12-05 15:49:09 -0500

    EBs are shooting projectiles correctly toward player

--- Assets/Scripts/Enemies/bossEB.cs
+++ Assets/Scripts/Enemies/bossEB.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class bossEB : MonoBehaviour
 {
     private bool _stopShooting = false;
+    private Transform _target;
 
     [SerializeField]
     GameObject _bossProjectile;
@@ -12,6 +13,12 @@ public class bossEB : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _target = GameObject.Find("Player").GetComponent<Transform>();
+        if (_target == null)
+        {
+            Debug.Log("_target on BossBolt is null!");
+        }
+
         if (_bossProjectile == null)
         {
             Debug.Log("_bossProjectile is missing!");
@@ -21,15 +28,24 @@ public class bossEB : MonoBehaviour
         {
             StartCoroutine(SpawnProjectile());
         }
-        
+
+
     }
 
     IEnumerator SpawnProjectile()
     {
+        yield return new WaitForSeconds(2.0f);
         while (_stopShooting == false)
         {
-            GameObject _bProjectile = Instantiate(_bossProjectile, transform.position, Quaternion.identity);
+            Vector3 _diff = (_target.position - this.transform.position);
+            _diff.Normalize();
+            float _lookRotation = Mathf.Atan2(_diff.y, _diff.x) * Mathf.Rad2Deg;
+            GameObject _bProjectile = Instantiate(_bossProjectile, transform.position, Quaternion.Euler(0f, 0f, (_lookRotation + 90)));
             yield return new WaitForSeconds(Random.Range(3.0f, 7.0f));
+            if(_target == null)
+            {
+                _stopShooting = true;
+            }
         }
 
     }
